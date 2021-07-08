e92da0c066c27c97f99ff27908b7c9846a1879a3
ant1code <tomas.martinek@uzh.ch>
2021-07-08 14:49:20 +0200

    really all the data now

---
+++ code r.R
@@ -0,0 +1,7 @@
+devtools::install_github("seanhardison1/pcs")
+
+m_profiles <- pcs::rider_profiles_men
+m_records <- pcs::rider_records_men
+
+w_profiles <- pcs::rider_profiles_women
+w_records <- pcs::rider_records_women
